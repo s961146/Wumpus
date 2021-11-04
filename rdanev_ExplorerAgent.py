934b5a47075820e386248ce77a6540096a055699
Radoslav Danev <radoslav.danev@gmail.com>
2021-11-04 19:59:19 -0400

    first commit

---
+++ rdanev_ExplorerAgent.py
@@ -0,0 +1,14 @@
+from wumpus import ExplorerAgent
+
+class KB():
+    def __init__(self):
+        pass
+
+class rdanev_ExplorerAgent(ExplorerAgent):
+    def __init__(self):
+        super().__init__()
+        self.kb = KB()
+
+
+    def program(self,percept):
+        pass
